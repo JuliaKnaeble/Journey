e69d15c4b2934a7bb730027fe3ec00295769f819
Julia.K <knaeble.julia@gmail.com>
2020-07-28 12:34:19 +0200

    Basic Nevbar setup

---
+++ src/Navbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="Navbar">
+      <p className="brand">
+        <span className="logo">
+          J<span className="dot-logo">.</span>
+        </span>
+        <span className="brand-name">Journey</span>
+      </p>
+      <p className="nav-item">Home</p>
+      <p className="nav-item">News</p>
+      <p className="nav-item">API</p>
+      <p className="nav-item">Download</p>
+      <button>Download Now</button>
+    </nav>
+  );
+};
+
+export default Navbar;
