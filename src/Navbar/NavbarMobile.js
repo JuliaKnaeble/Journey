ae70b7276352a4c068195641f5ad3005bd9e0954
Julia.K <knaeble.julia@gmail.com>
2020-08-05 16:20:03 +0200

    Started setting up mobile navigation

--- src/Navbar/NavbarMobile.js
+++ src/Navbar/NavbarMobile.js
@@ -1,24 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavbarMobile.css";
 
 const NavbarMobile = () => {
+  const [clicked, setClicked] = useState(false);
+
+  let menu;
+  if (clicked) {
+    menu = (
+      <div className="mobile-menu">
+        <img
+          src={require(`../Assets/app_logo.png`)}
+          alt=""
+          className="mobile-menu-items menu-logo"
+        />
+        <div className="mobile-menu-items">Home</div>
+        <div className="mobile-menu-items">News</div>
+        <div className="mobile-menu-items">Api</div>
+        <div className="mobile-menu-items">Contact</div>
+      </div>
+    );
+  }
+
+  function handleClick() {
+    setClicked(!clicked);
+  }
+
   return (
-    <div className="NavbarMobile">
-      <p className="brand-mobile">
-        <span>
-          <img
-            src={require(`../Assets/app_logo.png`)}
-            alt=""
-            className="logo-mobile"
-          />
-        </span>
-        <span className="brand-name-mobile">Journey</span>
-      </p>
-      <img
-        src={require(`../Assets/hamburger.png`)}
-        alt=""
-        className="burger-mobile"
-      />
+    <div>
+      <div className="NavbarMobile">
+        <p className="brand-mobile">
+          <span>
+            <img
+              src={require(`../Assets/app_logo.png`)}
+              alt=""
+              className="logo-mobile"
+            />
+          </span>
+          <span className="brand-name-mobile">Journey</span>
+        </p>
+        <img
+          src={
+            clicked
+              ? require(`../Assets/close_x.png`)
+              : require(`../Assets/hamburger.png`)
+          }
+          alt=""
+          className="burger-mobile"
+          onClick={handleClick}
+        />
+        {menu}
+      </div>
     </div>
   );
 };
