dc3c93c228087ce99d96a32d9fb269a6fa60fe4c
Julia.K <knaeble.julia@gmail.com>
2020-08-10 15:01:25 +0200

    Slide show with one item centered for mobile

--- src/MainContent/MobileDisplay.js
+++ src/MainContent/MobileDisplay.js
@@ -4,21 +4,27 @@ import "./MobileDisplay.css";
 const MobileDisplay = (props) => {
   return (
     <div className="MobileDisplay" style={props.style}>
-      <img
-        src={require(`../Assets/${props.mobileOne}.png`)}
-        alt=""
-        className="mobile side side-left"
-      />
-      <img
-        src={require(`../Assets/${props.mobileTwo}.png`)}
-        alt=""
-        className="mobile middle"
-      />
-      <img
-        src={require(`../Assets/${props.mobileThree}.png`)}
-        alt=""
-        className="mobile side side-right"
-      />
+      <div className="carousel-item">
+        <img
+          src={require(`../Assets/${props.mobileOne}.png`)}
+          alt=""
+          className="mobile side side-left"
+        />
+      </div>
+      <div className="carousel-item">
+        <img
+          src={require(`../Assets/${props.mobileTwo}.png`)}
+          alt=""
+          className="mobile middle"
+        />
+      </div>
+      <div className="carousel-item">
+        <img
+          src={require(`../Assets/${props.mobileThree}.png`)}
+          alt=""
+          className="mobile side side-right"
+        />
+      </div>
     </div>
   );
 };
